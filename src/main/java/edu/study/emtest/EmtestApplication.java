66f0ee35cfc910007f0d74a334ab8107dcbba552
Dmitriy Pudovkin <dmitriy.pudovkin@rtlabs.ru>
2023-02-28 07:41:16 +0300

    init commit

---
+++ src/main/java/edu/study/emtest/EmtestApplication.java
@@ -0,0 +1,13 @@
+package edu.study.emtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/study/emtest/EmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.study.emtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
